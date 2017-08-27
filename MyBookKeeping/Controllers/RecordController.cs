07ffbb8e3f30c7079eb97e15c0eb62c44f42d39f
Sean Liao <sseanliao7@gmail.com>
2017-08-27 16:29:45 +0800

    1. 更新 jQuery。 2. 將 jquery.unobtrusive-ajax.min.js 移至最後呼叫，以便觸發 Ajax 更新。 3. 更新 nuget 套件：Compiler。 4. 現在 Ajax 可正常運作。

--- MyBookKeeping/Controllers/RecordController.cs
+++ MyBookKeeping/Controllers/RecordController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using MyBookKeeping.Filters;
 using MyBookKeeping.Models;
 using MyBookKeeping.Models.DataPostModels;
 using MyBookKeeping.Models.ViewModels;
@@ -22,13 +23,8 @@ namespace MyBookKeeping.Controllers
             _recordService = new RecordService( unitOfWork );
         }
 
-        public ActionResult Index( int? page )
-        {
-            return View( );
-        }
-
         [HttpPost]
-        public ActionResult NewPost( AccountRecord input )
+        public ActionResult AjaxPosts( AccountRecord input )
         {
             if ( ModelState.IsValid )
             {
@@ -36,14 +32,27 @@ namespace MyBookKeeping.Controllers
                 _recordService.createNewRecord( accountbook );
                 _recordService.save( );
             }
-
-            return RedirectToAction( "Index" );
+            return RedirectToAction( "RenderAjaxPartialView" );
         }
 
-        public ActionResult RenderPosts( int? page )
+        public ActionResult Index( int? page )
+        {
+            return View( );
+        }
+
+        [AjaxOnly]
+        public ActionResult RenderAjaxPartialView( int? page )
         {
             page = page ?? 1;
             var pagedList = getIPagedList( page.Value );
+            return PartialView( "_IndexPartial", pagedList );
+        }
+
+        [ChildActionOnly]
+        public ActionResult RenderParialView( )
+        {
+            const int firstPage = 1;
+            var pagedList = getIPagedList( firstPage );
             return PartialView( "_IndexPartial", pagedList );
         }
 
@@ -51,6 +60,7 @@ namespace MyBookKeeping.Controllers
         {
             return _recordService.getRecords( )
                                  .OrderBy( x => x.Dateee )
+                                 .ThenBy( x => x.Id )
                                  .ProjectTo<RecordViewModel>( )
                                  .ToPagedList( page, _pageSize );
         }
