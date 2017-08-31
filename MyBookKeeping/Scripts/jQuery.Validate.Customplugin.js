091bf61ebdfb6df3dd083ba0889557a91dd2cc4d
Sean Liao <Sean_L1@ms1.hilosystems.tw>
2017-08-31 09:24:39 +0800

    1. 移除前端驗證不必要的判斷式。

--- MyBookKeeping/Scripts/jQuery.Validate.Customplugin.js
+++ MyBookKeeping/Scripts/jQuery.Validate.Customplugin.js
@@ -1,7 +1,4 @@
 ﻿$.validator.addMethod("positivenumber", function (value, element, param) {
-    if (value === false) {
-        return true;
-    }
     return value > 0 && value % 1 === 0 && value <= parseInt(param);
 });
 $.validator.unobtrusive.adapters.addSingleVal("positivenumber", "input");
